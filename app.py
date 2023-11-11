a118197c7751c21f60b0b9b97bf99dd9fee2a905
hnlxy <858865902@qq.com>
2023-11-11 22:12:47 +0800

    更改父模板文件'base.html'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = create_app()
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
