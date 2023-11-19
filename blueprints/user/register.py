5ea5bf8217a3589dea5105a38ebe3f0662536140
hnlxy <858865902@qq.com>
2023-11-19 14:40:02 +0800

    ajax实现注册功能

--- blueprints/user/register.py
+++ blueprints/user/register.py
@@ -1,20 +1,25 @@
 from blueprints.user import user_bp
-from flask import request, render_template, redirect, session, url_for
+from flask import request, render_template, redirect, session, url_for, jsonify
 from extensions import db
 from models import *
 
 
-@user_bp.route('/register', methods=["get", "post"])
+@user_bp.route('/register', methods=["GET", "POST"])
 def register():
-    # print(request.form)
     if request.method == "GET":
         return render_template('pages/user/register.html')
-    elif request.method == "POST":
-        admin = Admin(
-            username=request.form.get("username"),
-            password=request.form.get("password"),
-            email=request.form.get("email")
-        )
-        db.session.add(admin)
-        db.session.commit()
+    else:
+        print("Hello")
+        # data = request.get_json()
+        # print(data)
+        data = request.get_json()
+        print(data)
+        print(data is None)
+        # admin = Admin(
+        #     username=data["username"],
+        #     password=data["password"],
+        #     email=data["email"]
+        # )
+        # db.session.add(admin)
+        # db.session.commit()
         return redirect("/login")
